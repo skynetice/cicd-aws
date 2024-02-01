46f3633e4acb441e0ad08c779447e0cd1e0243b3
skynetice <skyneticj@gmail.com>
2024-02-01 15:15:04 +0700

    RPATB-5 <message>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "hehe22" }');
+    res.send('{ "response": "hehe22 jira" }');
 });
 
 app.get('/will', function (req, res) {
